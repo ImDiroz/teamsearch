5e39c8a173f484610648d57a66196e736ba5de60
Toggle12 <loltrymy321@gmail.com>
2021-06-04 09:36:02 +1000

    fixed signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -2,29 +2,41 @@ const System = require("../index.js").System;
 let router = require("express").Router();
 
 router.get("/", (request, response) => {
-        response.render("signup", {
-                title: "  TeamSearch | Регистрация"
-        });
+	response.render("signup", {
+			title: "  TeamSearch | Регистрация"
+	});
 });
 
 router.post("/", function (request, response) {
-	console.log(request.body);
-	if (request.body.email.trim() == "") response.set("Connection", "close");
-	if (request.body.login.trim() == "") response.set("Connection", "close");
-	if (request.body.password.trim() == "") response.set("Connection", "close");
-	if (request.body.password != request.body.passwordSecond) response.set("Connection", "close");
+	let errors = [];
 
-	System.bcrypt.hash(request.body.password, 10, (err, hash) => {
-        	 System.db.models.users.create({
-			email: request.body.email,
-			login: request.body.login,
-			password: request.body.password
-		 });
+	// validating
+
+	if (request.body.email.trim() == "") errors.push("Введите email");
+	if (request.body.login.trim() == "") errors.push("Введите login");
+	if (request.body.password.trim() == "") errors.push("Введите пароль");
+	if (request.body.password != request.body.passwordSecond) errors.push("Пароль не совпадает с подтверждением");
+	if (sequelize.models.posts.findOne({ where: { 
+		login: request.body.login,
+		email: request.body.email
+	 }}) != undefined) errors.push("Такой пользователь уже существует");
+
+	if (errors.length != 0) { // if errors was found 
+		response.send(errors[0]);
+		response.set("Connection", "close"); // closing connection
+	}
+
+	System.bcrypt.hash(request.body.password, 10, (err, hash) => { //crypting user password
+        	System.db.models.users.create({ // creating user
+				email: request.body.email,
+				login: request.body.login,
+				password: hash // hash of password
+		 	});
     	});
 
 	response.render("signup", {
-                title: "  TeamSearch | Успешная регистрация "
-        });
+            title: "  TeamSearch | Успешная регистрация "
+    });
 });
 
 module.exports = router;
