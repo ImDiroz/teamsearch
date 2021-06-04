7ac24a9e7343a4bf6d7d6c194dd780d4cfaf18bd
Toggle12 <loltrymy321@gmail.com>
2021-06-04 10:55:29 +1000

    fixed login. added profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const System = require("../index.js").System;
 let router = require("express").Router();
 
 router.get("/", (request, response) => {
-	response.render("progile", {
+	response.render("profile", {
 			title: "  TeamSearch | " + request.session.logged_user.login,
             session: request.session.logged_user
 	});
