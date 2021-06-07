1de0a4e8d0089acfd1a4e40a8dd6f268bfda501d
Toggle12 <loltrymy321@gmail.com>
2021-06-07 16:16:53 +1000

    fixed handlebars helpers

--- handlebars.js
+++ handlebars.js
@@ -1,4 +1,4 @@
-let Handlebars = require("index.js").System.hbs;
+let Handlebars = require("./index.js").System.hbs;
 
 let handlebarsCreateHelpers = module.exports.handlebarsCreateHelpers = () => {
     Handlebars.registerHelper('ifeq', function (a, b, options) {
